cf8dd2f433dcb856b6c8d55e08a7a35feef6c420
Aliakbar Kadkhoda <aliakbarkadkhoda137818@hotmail.com>
2025-05-14 20:29:31 +0330

    update achivement section didnt render the earth now fixed

--- src/components/hompage/Achivement.tsx
+++ src/components/hompage/Achivement.tsx
@@ -1,8 +1,48 @@
 const Achivement = () => {
   return (
-    <div className="w-full bg-pink-1 mt-[144px] px-[346px]  py-16 h-[546px]">
-      <div className=" flex py-3 px-[3px]">
-        <div className="w-1/2"></div>
+    <div
+      className="w-full  mt-[144px] px-[346px] py-16 h-[546px] "
+      style={{ backgroundImage: 'url("/Avesta achievements.png")' }}
+    >
+      <div className=" flex py-3 px-[3px] gap-x-[176px] ">
+        <div className="w-1/2 h-[394px] ">
+          <div className="h-full pr-[62px] relative">
+            <img
+              src="/Ellipse 4.png"
+              className="absolute top-[149px] left-[241px] right-[90.35px] bottom-[123.3px] transform -translate-x-1/2    "
+            />
+            <div className="absolute top-[13px] left-[152px] right-[188px] bottom-[307px] flex flex-col justify-center items-center  text-shadow-[0px_4px_4px_rgba(0,0,0,0.5)] ">
+              <div dir="ltr" className="text-purpel-6  text-[32px]">
+                +1453
+              </div>
+              <div className="text-purpel-7 text-[20px]">فرند کشتی</div>
+            </div>
+            <div className="absolute flex flex-col justify-center items-center top-[50px] left-[345px] right-[14px] bottom-[238px] text-shadow-[0px_4px_4px_rgba(0,0,0,0.5)]">
+              <div dir="ltr" className="text-purpel-6  text-[32px]">
+                +6106
+              </div>
+              <div className="text-purpel-7 text-[20px]">ثبت واگن</div>
+            </div>
+            <div className="absolute flex flex-col justify-center items-center top-[308px] left-[143px] right-[197px] bottom-[12px] text-shadow-[0px_4px_4px_rgba(0,0,0,0.5)]">
+              <div dir="ltr" className="text-purpel-6  text-[32px]">
+                +56419603
+              </div>
+              <div className="text-purpel-7 text-[20px]">تناژ تخلیه تاکنون</div>
+            </div>
+            <div className="absolute flex flex-col justify-center items-center  top-[259px] left-[338px] right-[7px] bottom-[61px]  text-shadow-[0px_4px_4px_rgba(0,0,0,0.5)]">
+              <div dir="ltr" className="text-purpel-6  text-[32px]">
+                +872
+              </div>
+              <div className="text-purpel-7 text-[20px]">ثبت کامیون</div>
+            </div>
+            <div className="absolute flex flex-col justify-center items-center  top-[154px] left-[43px] right-[248px] bottom-[166px] text-shadow-[0px_4px_4px_rgba(0,0,0,0.5)]">
+              <div dir="ltr" className="text-purpel-6  text-[32px]">
+                +138
+              </div>
+              <div className="text-purpel-7 text-[20px]">صاحبان کالا</div>
+            </div>
+          </div>
+        </div>
         <div className="w-1/2 flex flex-col gap-y-6">
           <span className="text-[32px] font-bold text-black-1">
             دستاوردهای شرکت <span className="text-orange-1"> اَوستا‌ </span>

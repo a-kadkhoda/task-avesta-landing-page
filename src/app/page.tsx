f2b2f24ca5ee15c0a8f8bacf4b69e2a63f08a208
Aliakbar Kadkhoda <aliakbarkadkhoda137818@hotmail.com>
2025-05-14 18:26:14 +0330

    footer done but two sections still need to add the feedpack and achivement section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Certificates from "@/components/hompage/Certificates";
 import Hero from "@/components/hompage/Hero";
 import NextStep from "@/components/hompage/NextStep";
 import Services from "@/components/hompage/Services";
+import { menu } from "@/lib/helper/navbar";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -25,10 +27,73 @@ export default function Home() {
         </span>
       </div>
       <div
-        className="h-[620px] w-full"
+        className="h-[620px] w-full pt-12 px-[175px]"
         style={{ backgroundImage: 'url("/footer.png")' }}
       >
-        <div></div>
+        <div className="flex  gap-x-[424px] h-[427px]">
+          <div className="flex flex-col gap-y-8 h-full">
+            <div className="w-[132px] h-[76px] pl-[6px]">
+              <img src="/logo.svg" className="size-full" />
+            </div>
+            <div className="flex  w-full gap-x-6">
+              <div className="w-1/3 text-[18px] text-black-1 ">
+                <span className="text-orange-1">منو</span>
+                <div className="flex flex-col mt-8">
+                  {menu.map((item, index) => {
+                    return (
+                      <Link
+                        key={index}
+                        href="/#"
+                        className="hover:text-orange-1"
+                      >
+                        {item}
+                      </Link>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="w-1/3  text-[18px] text-black-1">
+                <span className="text-orange-1">دغتر تهران</span>
+                <div className="mt-8">
+                  <p>
+                    دفتر تهران طرشت ، مترو دانشگاه شریف خیابان تیموری نرسیده به
+                    اتوبان یادگار امام کوچه عموزاده، پ۸ واحد۲
+                  </p>
+                  <span>تلفن : 66055124 - 021</span>
+                </div>
+              </div>
+              <div className="w-1/3  text-[18px] text-black-1">
+                <span className="text-orange-1">دغتر خوزستان</span>
+                <div className="mt-8">
+                  <p>
+                    دفتر خوزستان بندرامام خمینی (ره) اداره کل بنادر و دریانوردی
+                    ، مجتمع بندری امام خمینی (ره) اسکله 20
+                  </p>
+                  <span>تلفن : 66055124 - 021</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="min-w-[210px] relative ">
+            <span className="text-orange-1 text-[22px] font-medium absolute -right-10 top-5">
+              دفتر تهران
+            </span>
+            <span className="text-orange-1 text-[22px] font-medium absolute bottom-10 left-14">
+              دفتر خوزستان
+            </span>
+          </div>
+        </div>
+        <div className="h-px bg-purpel-5 my-[49px]"></div>
+        <div className="flex justify-between">
+          <div className="flex flex-row-reverse gap-4">
+            <img src="/youtube.png" className="cursor-pointer" />
+            <img src="/instagram.png" className="cursor-pointer" />
+            <img src="/linkedin.png" className="cursor-pointer" />
+          </div>
+          <span dir="ltr" className="">
+            AvestaSmart @ 2025. All rights reserved.
+          </span>
+        </div>
       </div>
     </div>
   );
